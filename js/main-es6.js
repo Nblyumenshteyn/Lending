7765b6179ef1f4fed8f56ea16fccf7ac985e94db
Natalya Blyumenshteyn <natalya.blyumenshteyn@dualbootpartners.com>
2022-09-14 23:46:32 +0400

    optimisation

--- js/main-es6.js
+++ js/main-es6.js
@@ -19,12 +19,21 @@
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header_nav');
   const menuCloseItem = document.querySelector('.header_nav-close');
+  const menuLinks = document.querySelectorAll('.header_link');
   burgerItem.addEventListener('click', () => {
     menu.classList.add('header_nav-active');
   })
   menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('header_nav-active');
   })
+  //если ширина экрана меньше, чем 767, при нажатии на ссылку меню, оно должно скрыться
+  if (window.innerWidth <= 767) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove('header_nav-active')
+      })
+    }
+  }
 }());
 
 //Scroll to anchors
